47f0b82bf09fad97393e8c9fd88a2abc1dc58309
Hugo Giraudel <hugo.giraudel@gmail.com>
2017-10-14 15:18:36 +0200

    Add loading spinner

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const ora = require('ora')
 const { reduce } = require('asyncro')
 const program = require('./program')
 const display = require('./display')
@@ -6,11 +7,15 @@ const evaluate = require('./evaluate')
 const dependencies = getDependencies(program)
 const packageNames = Object.keys(dependencies)
 const evaluator = evaluate(dependencies, program.pr)
+const spinner = ora('Evaluating dependencies…').start()
 
 reduce(packageNames, evaluator)
-  .then(entries => ({
-    unsafe: entries.filter(entry => !entry.safe),
-    safe: entries.filter(entry => entry.safe)
-  }))
-  .then(display)
+  .then(entries => {
+    spinner.stop()
+
+    return display({
+      unsafe: entries.filter(entry => !entry.safe),
+      safe: entries.filter(entry => entry.safe)
+    })
+  })
   .catch(console.error.bind(console))
